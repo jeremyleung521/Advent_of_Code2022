8452af857100c420dc826a40a2ca976c28749693
Jeremy Leung <jml230@pitt.edu>
2022-12-01 22:31:04 -0500

    new perl day and python DayX up

--- python/DayX/DayX.py
+++ python/DayX/DayX.py
@@ -40,15 +40,15 @@ def return_top3(input_list):
 
 def main():
     ## Part 1
-    #b = read_input("Day1_test_input.txt")
-    b = read_input("Day1_input.txt")
+    #b = read_input("DayX_test_input.txt")
+    b = read_input("DayX_input.txt")
     answer = return_max(b)
     print(f'Elf {answer[1]+1} has {answer[0]} calories worth of food.')
 
 def main2():
     # Part 2
-    # a = read_input("Day1_test_input.txt")
-    a = read_input("Day1_input.txt")
+    # a = read_input("DayX_test_input.txt")
+    a = read_input("DayX_input.txt")
     answer2 = return_top3(a)
     print(f'Elves {answer2[1] + 1} have {answer2[0]} calories worth of food.')
 
