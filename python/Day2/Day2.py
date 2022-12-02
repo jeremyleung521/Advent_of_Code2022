0c74af3f92bb32f700884b58dfcd3102017dbce0
Jeremy Leung <jml230@pitt.edu>
2022-12-02 12:12:36 -0500

    Day2 Solutions

--- python/Day2/Day2.py
+++ python/Day2/Day2.py
@@ -10,7 +10,7 @@ from perf import calc_perf
 
 chose_dict = {'X': 1, 'Y':2, 'Z':3, 'A':1, 'B':2, 'C': 3} # (X,A) is rock, (Y,B) is paper, (Z,C) is scissors
 point_dict = {0:3, 1:0, 2:6, -1:6, -2: 0} # Diff 0 == Draw (3 points), Diff 2 or -1 == Win (6 points), Diff 1 or -2 == Lose (0 points)
-condition_dict = {'X':[]} # X: Lose, Y: Draw, Z:Win
+condition_dict = {'XA':'Z', 'XB':'X', 'XC':'Y', 'YA':'X', 'YB':'Y', 'YC':'Z', 'ZA':'Y', 'ZB':'Z', 'ZC':'X' } # X: Lose, Y: Draw, Z:Win
 
 
 def loop_point(input_list):
@@ -22,11 +22,8 @@ def loop_point(input_list):
 
 def fig_move(input_list):
     count = 0
-    for pair in input_list:
-        
-        # X: Lose, Y: Draw, Z: Win
-    
-    
+    for idx, pair in enumerate(input_list):
+        input_list[idx,1] = condition_dict[pair[1]+pair[0]]
     return input_list
 
 def main():
@@ -38,15 +35,16 @@ def main():
 
 def main2():
     # Part 2
-    # a = read_input("Day2_test_input.txt")
-    #a = read_input("Day2_input.txt")
-    #answer2 = return_top3(a)
-    #print(f'Elves {answer2[1] + 1} have {answer2[0]} calories worth of food.')
-    pass
+    #a = numpy.loadtxt("Day2_test_input.txt", dtype=str)
+    a = numpy.loadtxt("Day2_input.txt", dtype=str)
+    answer2 = fig_move(a)
+    answer2 = loop_point(answer2)
+    print(f'I won {answer2} number of points following this guide.')
+
 
 if __name__ == "__main__":
     ## Part 1
     calc_perf(main())
 
     ## Part 2
-    #calc_perf(main2())
+    calc_perf(main2())
