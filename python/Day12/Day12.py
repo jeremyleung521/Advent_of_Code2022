0b8d6d8d83fed79af0f28bf8ade9262187feb365
Jeremy Leung <jml230@pitt.edu>
2022-12-12 13:13:58 -0500

    Day12 Solutions Clean up (Part 1/2 use same code)

--- python/Day12/Day12.py
+++ python/Day12/Day12.py
@@ -25,27 +25,6 @@ def read_input(file_name):
     final_array[end[0][0], end[1][0]] = 122
     return final_array, (start[0][0], start[1][0]), (end[0][0], end[1][0])
 
-def calc_possible_moves(input_array):
-    # cost_array = numpy.zeros((len(a), len(a[0])))
-    adj_array = dict({})
-    index = [(-1,0),(1,0), (0,-1),(0,1)]
-    for row in range(0, len(input_array)):
-        for column in range(0, len(input_array[0])):
-            candidates = []
-            for diff in index:
-                # If neighbour diff is too high not working
-                try:
-                    check = (row+diff[0],column+diff[1])
-                    if any(i<0 for i in check):
-                        pass
-                    elif input_array[row+diff[0],column+diff[1]] <= input_array[row,column] + 1:
-                        candidates.append((row + diff[0], column + diff[1]))
-                except IndexError:
-                    pass
-
-            adj_array[(row,column)] = candidates
-
-    return adj_array
 
 def calc_possible_moves_inverse(input_array):
     # cost_array = numpy.zeros((len(a), len(a[0])))
@@ -95,7 +74,7 @@ def bfs_optimize(adj_array, start_x, start_y, end):
         for n in adj_array[u]:
             # print(n)
             if u in end:
-                print(d[u])
+                print(f'Shortest path is {d[u]} steps.')
                 return parent, d
             if n not in d:
                 if n not in parent:
@@ -109,13 +88,8 @@ def main():
     ## Part 1
     # b, start_index, end_index = read_input("Day12_test_input.txt")
     b, start_index, end_index = read_input("Day12_input.txt")
-    adj_array = calc_possible_moves(b)
-
-    # print(adj_array)
-
-    parent, dist = bfs_optimize(adj_array, start_index[0], start_index[1], [end_index])
-
-    # print(parent,dist)
+    adj_array = calc_possible_moves_inverse(b)
+    parent, dist = bfs_optimize(adj_array, end_index[0], end_index[1], [start_index])
 
 def main2():
     # Part 2
@@ -124,7 +98,6 @@ def main2():
     adj_array = calc_possible_moves_inverse(a)
 
     possible_starts = find_possible_starts(a)
-    # print(adj_array)
 
     parent, dist = bfs_optimize(adj_array, end_index[0], end_index[1], possible_starts)
 
