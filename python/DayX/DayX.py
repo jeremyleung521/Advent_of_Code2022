731ae38dfe428ee33cba87cbf7c9ef38480eb999
Jeremy Leung <jml230@pitt.edu>
2022-12-18 23:49:36 -0500

    DayX update

--- python/DayX/DayX.py
+++ python/DayX/DayX.py
@@ -2,10 +2,9 @@
 #
 # First attempt at doing Day X of Advent of Code 2022
 
-import sys
-sys.path.append("..")
+import time
+import numpy
 
-from perf import calc_perf
 
 def read_input(file_name):
     with open(file_name, 'r') as f:
@@ -24,25 +23,29 @@ def read_input(file_name):
 
     return final_count
 
+
 def return_max(input_list):
     max_cal = numpy.max(input_list)
     where = numpy.where(input_list == max_cal)[0][0]
 
     return [int(max_cal), where]
 
+
 def return_top3(input_list):
-    sorted_list = sorted(input_list, key=lambda x:-x)
+    sorted_list = sorted(input_list, key=lambda x: -x)
     cal_sum = sum(sorted_list[0:3])
     where_three = numpy.where(input_list > sorted_list[3])[0]
 
     return [int(cal_sum), where_three]
 
+
 def main():
     ## Part 1
-    #b = read_input("DayX_test_input.txt")
+    # b = read_input("DayX_test_input.txt")
     b = read_input("DayX_input.txt")
     answer = return_max(b)
     print(f'Elf {answer[1]+1} has {answer[0]} calories worth of food.')
+
 
 def main2():
     # Part 2
@@ -51,9 +54,14 @@ def main2():
     answer2 = return_top3(a)
     print(f'Elves {answer2[1] + 1} have {answer2[0]} calories worth of food.')
 
+
 if __name__ == "__main__":
     ## Part 1
-    calc_perf(main())
+    startTime = time.perf_counter()
+    main()
+    print(f'{time.perf_counter() - startTime} sec.')
 
     ## Part 2
-    calc_perf(main2())
+    startTime = time.perf_counter()
+    main2()
+    print(f'{time.perf_counter() - startTime} sec.')
