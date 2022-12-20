6073f9bb5df4955fb032d1bff2d2b053dccb5490
Jeremy Leung <jml230@pitt.edu>
2022-12-20 13:15:56 -0500

    Day20 Working

--- python/Day20/Day20.py
+++ python/Day20/Day20.py
@@ -1,6 +1,8 @@
 # Day20.py
 #
 # First attempt at doing Day 20 of Advent of Code 2022
+# With help from here:
+# https://topaz.github.io/paste/#XQAAAQArBQAAAAAAAAARiEJHiiMzw3cPM/1Vl+2nx/DqKkM2yi+HVdpp+qLiqZwdO8DftYzG7xETHPj1qjypxh5M1TcPUMscdwJ9eMao1h4KjZSFqvUVGgRO1B0UY9xy1ppiz5MCls8P3jkcK0nuQ0xyqjMGO1zjudp0iPknFmDdndniTzqJ7bQyJOivVMdvFt6PZvBFRLBJlJt+W2OfKFao4pmwqJQ3tYnoeqoxaZBRq6SnzFKevZL5mczBvlhZ4Z7pOYZWJod3DUZB62OVqwdbVuZ1Gf/YtwiEH73ZCQkNYCTDsmM7Q+7KoPCx1BL7vC8nnymRszz7wQoPgnExffEtXCUA5BrwGtgN+iC7bkvBbLKdGgBlxLpXCxv9tDHXSTAAlW0cfwiiOBGFGYvptqGAEjKFqKzBPe2V7Krsp5rrGFg6b0y5oV3Dzm1Z9SNgK1GRxvQQxGPgcmcSUk3isUQm5j4aqcghwu3d7GkrKCT7kX1dW2YUymL8viTMxauPyqK6xrBIU81Uw0ArXN9KS90ssoDvQ+9X610H8rOxseIchLFxlqY2ldEL8mH6eXvAO6QgiP3cwrN84XRq/v/TJTRFAORAP1r1O3xF19rHw4BxW5jSEbNsnIMVqn3/F/8Vro5zSdIKib6VyuKjofLB/JRqD+5qt2TQi5xqyUOFeWDZeMcDj4Xoc95wKxdPOa1H1wdVL6bIWJDXNJ6R+FBxNpU1rsX7PZv7YxWWeIqICaArC51izABqc0jQLjKIqCIkG0TUQSX1WCENuzO2CZcZKp7fN4YmIWBN//fit2w=
 
 import time
 import numpy
@@ -8,75 +10,66 @@ from copy import deepcopy
 from collections import deque
 
 
-def read_input(file_name):
+def read_input(file_name, key=1):
     with open(file_name, 'r') as f:
         read_list = f.read().splitlines()
 
-    final_list = [int(i) for i in read_list]
+    final_list = [(idx, int(i)*key) for idx, i in enumerate(read_list)]
 
-    return final_list
+    return deque(final_list)
 
-
-def loop(final_list):
-    copy_list = deepcopy(final_list)
-    mod = len(copy_list)
-    neighbor_list = dict()
-    for idx, val in enumerate(copy_list):
-        neighbor_list[val] = [idx, copy_list[(idx-1) % mod], copy_list[(idx+1) % mod]]
-
-    for idx in range(len(copy_list)):
-        val = copy_list[idx]
-        curr_pos = final_list.index(copy_list[idx]) # This doesn't account for duplicates
-        # Doesn't work...
-        while True:
-            if final_list[(curr_pos-1) % mod] != copy_list[(curr_pos-1) % mod] and \
-                final_list[(curr_pos-1) % mod] != copy_list[(curr_pos-1) % mod]:
-                curr_pos = final_list.index(copy_list[idx], (curr_pos +1 % mod))
-            else:
-                break
-        if val < 0:
-            new_pos = (curr_pos + val) % mod -1
-        else:
-            new_pos = (curr_pos + val) % mod
-        print('new_iter')
-        print(curr_pos, new_pos)
-        print('looking at: ', val)
-
-        if new_pos == (idx % mod):
-            pass
-        else:
-            final_list.remove(val)
-            print(final_list)
-            final_list.insert(new_pos, val)
-        print(final_list)
 
 def find_character(final_list, n):
-    start = final_list.index(0)
-    # print(start)
+    while final_list[0][1] != 0:
+        final_list.rotate(-1)
+
     mod = len(final_list)
-    print(len(final_list))
     answer = []
     for i in n:
-        answer.append(final_list[(start + i) % mod])
+        answer.append(final_list[i % mod][1])
 
     # print(answer)
     return answer, sum(answer)
 
+def decrypt_nums(nums):
+    # print(nums)
+    mod = len(nums)
+    for idx in range(mod):
+        while idx != nums[0][0]:
+            nums.rotate(-1)
+
+        i, n = nums.popleft()
+        nums.rotate(-n)
+        nums.appendleft((i, n))
+        nums.rotate(n)
+
+
+def basic_decrypt_sum(nums):
+    decrypt_nums(nums)
+    answer, answer2 = find_character(nums, [1000,2000,3000])
+    return answer, answer2
+
+
 def main():
     ## Part 1
-    b = read_input("Day20_test_input.txt")
-    # b = read_input("Day20_input.txt")
-    loop(b)
-    answer, sum_answer = find_character(b, [1000, 2000, 3000])
-    print(f'Sum of {answer} is {sum_answer}.')
+    # b = read_input("Day20_test_input.txt")
+    b = read_input("Day20_input.txt")
+    answer, answer2 = basic_decrypt_sum(b)
+    # print(sorted(b, key=lambda x:x[0]))
+    # answer, sum_answer = find_character(answer, [1000, 2000, 3000])
+    print(f'Sum of {answer} is {answer2}.')
 
 
 def main2():
     # Part 2
-    a = read_input("Day20_test_input.txt")
-    # a = read_input("Day20_input.txt")
-    answer2 = return_top3(a)
-    print(f'Elves {answer2[1] + 1} have {answer2[0]} calories worth of food.')
+    # a = read_input("Day20_test_input.txt", 811589153)
+    a = read_input("Day20_input.txt", 811589153)
+
+    # Loop through 10 times
+    for _ in range(10):
+        answer, answer2 = basic_decrypt_sum(a)
+
+    print(f'Sum of {answer} is {answer2}.')
 
 
 if __name__ == "__main__":
@@ -86,6 +79,6 @@ if __name__ == "__main__":
     print(f'{time.perf_counter() - startTime} sec.')
 
     ## Part 2
-    #startTime = time.perf_counter()
-    #main2()
-    #print(f'{time.perf_counter() - startTime} sec.')
+    startTime = time.perf_counter()
+    main2()
+    print(f'{time.perf_counter() - startTime} sec.')
