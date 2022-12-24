08fb604bd315973dc3913ce8f39ca3276931598a
Jeremy Leung <jml230@pitt.edu>
2022-12-23 21:27:34 -0500

    Day23 Speedup!

--- python/Day23/Day23.py
+++ python/Day23/Day23.py
@@ -6,7 +6,7 @@ import time
 import numpy
 from operator import add, itemgetter
 from collections import deque
-from tqdm.auto import tqdm, trange
+from tqdm.auto import trange
 
 
 def read_input(file_name):
@@ -19,45 +19,42 @@ def read_input(file_name):
 
     n_elves = len(curr_occupied)
 
-    neighbour = dict()
-
     # print(f'Initialized: {curr_occupied}')
 
     # N, S, W, E, NE, NW, SE, SW
     directions = numpy.asarray([(-1, 0), (1, 0), (0, -1), (0, 1), (-1, 1), (-1, -1), (1, 1), (1, -1)])
 
-    consideration = deque([[0, 4, 5], [1, 6, 7], [2, 5, 7], [3, 4, 6]]) # Checks for N/S/W/E moves
+    consideration = deque([[0, 4, 5], [1, 6, 7], [2, 5, 7], [3, 4, 6]])  # Checks for N/S/W/E moves
     order = deque([0, 1, 2, 3])  # N, S, W, E
 
-    return list_of_lists, curr_occupied, neighbour, n_elves, directions, consideration, order
+    return list_of_lists, curr_occupied, n_elves, directions, consideration, order
 
 
-def propagate(curr_occupied, neighbour, directions, consideration, order):
+def propagate(curr_occupied, directions, consideration, order):
     # Refresh Neighbour List
-    neighbour.clear()
-    for j in curr_occupied:
-        neighbour[j] = []
+    # neighbour = dict()
+    # for j in curr_occupied:
+    #     neighbour[j] = []
 
     new_location = {}
     count = 0
     flag = False
     for coord in curr_occupied:
         # Create neighbours map
-        check_c = []
-        for direction in directions:
-            check = tuple(map(add, coord, direction))
-            check_c.append(check)
-            if check in curr_occupied:
-                neighbour[coord].append(check)
+        # check_c = [(coord[0]+dx, coord[1]+dy) for (dx, dy) in directions]
+        check_bool = [False for _ in range(8)]
+        for idx, direction in enumerate(directions):
+            if tuple(map(add, coord, direction)) in curr_occupied:
+                check_bool[idx] = True
 
-        if not neighbour[coord]:  # If no neighbours, just stay in place
+        if not any(check_bool):  # If no neighbours, just stay in place
             # print('error where')
             count += 1
             new_location[coord] = coord
         else:
             for check_list, output in zip(consideration, order):
-                check_check = [check_c[i] for i in check_list]
-                if all(coord_c not in curr_occupied for coord_c in check_check):
+                check_check = [check_bool[i] for i in check_list]
+                if not any(check_check):
                     new_location[coord] = tuple(map(add, coord, directions[output]))
                     # print(directions[output])
                     break
@@ -69,6 +66,7 @@ def propagate(curr_occupied, neighbour, directions, consideration, order):
     if count == len(curr_occupied):
         # print('error here')
         flag = True
+        return new_location, curr_occupied, flag
     # print(new_location)
 
     # Taking care of duplicates, Reject duplicates
@@ -83,7 +81,7 @@ def propagate(curr_occupied, neighbour, directions, consideration, order):
             new_location[key] = key
 
     # Answers and prep for next iteration
-    curr_occupied = list(new_location.values())
+    curr_occupied = set(new_location.values())
     consideration.rotate(-1)
     order.rotate(-1)
 
@@ -114,28 +112,26 @@ def find_minmax(curr_occupied, n_elves, visualize=False):
 
 def main():
     ## Part 1
-    # initialize, curr_occupied, neighbour, n_elves, directions, conditions, order = read_input("Day23_smaller_input.txt")
-    # initialize, curr_occupied, neighbour, n_elves, directions, conditions, order = read_input("Day23_test_input.txt")
-    initialize, curr_occupied, neighbour, n_elves, directions, conditions, order = read_input("Day23_input.txt")
+    # initialize, curr_occupied, n_elves, directions, conditions, order = read_input("Day23_smaller_input.txt")
+    # initialize, curr_occupied, n_elves, directions, conditions, order = read_input("Day23_test_input.txt")
+    initialize, curr_occupied, n_elves, directions, conditions, order = read_input("Day23_input.txt")
 
     for _ in trange(10):
-        new_location, curr_occupied, flag = propagate(curr_occupied, neighbour, directions, conditions, order)
+        new_location, curr_occupied, flag = propagate(curr_occupied, directions, conditions, order)
         answer = find_minmax(curr_occupied, n_elves)
-        if flag is True:
-            print(idx)
 
     print(f'There are {answer} empty spots in the minimum rectangle.')
 
 
 def main2():
     # Part 2
-    # initialize, curr_occupied, neighbour, n_elves, directions, conditions, order = read_input("Day23_smaller_input.txt")
-    # initialize, curr_occupied, neighbour, n_elves, directions, conditions, order = read_input("Day23_test_input.txt")
-    initialize, curr_occupied, neighbour, n_elves, directions, conditions, order = read_input("Day23_input.txt")
+    # initialize, curr_occupied, n_elves, directions, conditions, order = read_input("Day23_smaller_input.txt")
+    # initialize, curr_occupied, n_elves, directions, conditions, order = read_input("Day23_test_input.txt")
+    initialize, curr_occupied, n_elves, directions, conditions, order = read_input("Day23_input.txt")
 
-    for idx in trange(1000000):
-        new_location, curr_occupied, flag = propagate(curr_occupied, neighbour, directions, conditions, order)
-        # answer = find_minmax(curr_occupied, n_elves)
+    for idx in trange(100000):
+        new_location, curr_occupied, flag = propagate(curr_occupied, directions, conditions, order)
+        # answer = find_minmax(curr_occupied, n_elves, visualize=True)
         if flag is True:
             print(idx+1)
             answer2 = idx+1
@@ -146,9 +142,9 @@ def main2():
 
 if __name__ == "__main__":
     ## Part 1
-    # startTime = time.perf_counter()
-    # main()
-    # print(f'{time.perf_counter() - startTime} sec.')
+    startTime = time.perf_counter()
+    main()
+    print(f'{time.perf_counter() - startTime} sec.')
 
     ## Part 2
     startTime = time.perf_counter()
